630929f6142a16d07e266d0a2b6e89faa1727704
rtsiabes <ra.chavez@outlook.com>
2025-04-12 01:55:29 -0600

    frmr

--- uasset_json_mod/AddFarmerStructures.js
+++ uasset_json_mod/AddFarmerStructures.js
@@ -1,22 +1,7 @@
-var fs = require('fs');
-const { addAssetsToJsonFile } = require('./utils');
-
-
-let dataArrayName = "Structures";
-let importsClassName = "StaticMesh";
-
-function getPushObject(outerIndex) {
-    return {
-        "$type": "UAssetAPI.PropertyTypes.Objects.ObjectPropertyData, UAssetAPI",
-        "Name": "0",
-        "DuplicationIndex": 0,
-        "IsZero": false,
-        "Value": outerIndex - 1, // Refers to the ObjectName in Imports
-    }
-}
-
+import { write } from "./FarmerStructures.js";
 
 const filePath = String.raw`C:\my\UE_modding\FModel\Output\Exports\Goat2\Content\Blueprints\GoatGear\Abilities\BP_GA_Builder.json`;
 let assetsFile = String.raw`C:\my\UE_modding\customassets4.txt`;
-let assets =  fs.readFileSync(assetsFile).toString().split("\n").map((line) => line.trim()).filter((line) => line.length > 0).filter(line => !line.startsWith('#'));
-addAssetsToJsonFile(filePath, assets, true, dataArrayName, importsClassName, getPushObject);
+
+write(filePath, assetsFile);
+
